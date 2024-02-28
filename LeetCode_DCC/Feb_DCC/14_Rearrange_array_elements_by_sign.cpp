902b06be3b046c89421252ab106e8ee5c5c52ae7
deeksha <pdeeksha1403@gmail.com>
2024-02-28 22:02:33 +0530

    DCC

--- LeetCode_DCC/Feb_DCC/14_Rearrange_array_elements_by_sign.cpp
+++ LeetCode_DCC/Feb_DCC/14_Rearrange_array_elements_by_sign.cpp
@@ -3,27 +3,25 @@ using namespace std;
 
 /*
 Logic:
-(1) The order of alternate positive and negative has to be maintained , so initialise positive_index with 0 and negative_index with 1.
-(2) Run a loop untill both the indexes are less than size of input array
-    if element at positiveindex is greater than zero , increment positve index with 2
-    if element at negative index is less than zero , increment negative index by 2
-    else swap the elements at positive and negative indexes.
+(1) The order of alternate positive and negative has to be maintained , so initialise positive_index with 0 and negative_index with 1 and an empty answer vector.
+(2) Run a loop untill you reach the end of input array
+    if element at index is greater than zero , set answer vector's postive_index as current input array index, and increment postive_index by 2.
+    else set answer vector's negative_index as current input array index, and increment negative_index by 2.   
 (3) Return the array.
 
 Complexity Analysis:
-(1) TC: O(N)
-(2) SC: O(1)
+(1) TC: O(n)
+(2) SC: O(n)
 */
 
 vector<int> rearrangeArray(vector<int>& nums) 
 {
     int positive_ind=0,negative_ind=1;
-    while(positive_ind<nums.size() && negative_ind<nums.size())
-    {
-        if(nums[positive_ind]>0)   positive_ind+=2;
-        else if(nums[negative_ind]<0)  negative_ind+=2;
-        else swap(nums[positive_ind],nums[negative_ind]);
-    }
-    return nums;
-    
+        vector<int> ans(nums.size());
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0)   { ans[positive_ind]=nums[i];positive_ind+=2;   }   
+            else            { ans[negative_ind]=nums[i];negative_ind+=2;   }
+        }
+        return ans;
 }
